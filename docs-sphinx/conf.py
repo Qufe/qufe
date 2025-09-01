d372c71e9aa85188fb242f32f0f8848404f52104
Qufe <bong9s@proton.me>
2025-09-01 12:25:38 +0900

    correct version no.

--- docs-sphinx/conf.py
+++ docs-sphinx/conf.py
@@ -9,7 +9,7 @@
 project = 'qufe'
 copyright = '2025, Qufe'
 author = 'Qufe'
-release = '0.1.3'
+release = '0.2.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
